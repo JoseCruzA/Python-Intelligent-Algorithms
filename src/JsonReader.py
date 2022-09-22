b9bfcdac382a21bacb7a77d5d63f6034f0221c69
Jose Cruz <josedanielcrz@gmail.com>
2022-09-22 17:27:09 -0500

    Delete unnecessary variable

--- src/JsonReader.py
+++ src/JsonReader.py
@@ -6,12 +6,12 @@ class Json:
     def __init__(self, path):
         self.path = path
 
-    def read(self, index):
+    def read(self):
         with open(self.path) as file:
             data = json.load(file)
 
-            return (data[index]["axis_x"]["name"], 
-                data[index]["axis_y"]["name"], 
-                data[index]["axis_x"]["data"], 
-                data[index]["axis_y"]["data"], 
-                data[index]["dataToPredict"])
+            return (data["axis_x"]["name"], 
+                data["axis_y"]["name"], 
+                data["axis_x"]["data"], 
+                data["axis_y"]["data"], 
+                data["dataToPredict"])
