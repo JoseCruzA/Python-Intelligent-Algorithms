b6cbfcb654ddf3ece26db0b0622925aabdfb7c4b
Jose Cruz <josedanielcrz@gmail.com>
2022-09-23 09:16:41 -0500

    Improve the UI

--- gui/GUI.py
+++ gui/GUI.py
@@ -1,9 +1,8 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter.filedialog import askopenfile
-from turtle import width
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg as figureCanvas
-
 from src.JsonReader import Json
 from src.regression import Regression
 
@@ -14,8 +13,11 @@ class GUI:
         self.frameBtn = tk.Frame(self.root, bg="white")
         self.frameGraph = tk.Frame(self.root, bg="white")
         self.canvas = None
+        self.table = None
+        self.dataToPredict = None
+        self.predictedData = None
         
-        self.frameBtn.pack(side=tk.LEFT, expand=True)
+        self.frameBtn.pack(side=tk.LEFT, expand=True, padx=20)
         self.frameGraph.pack(side=tk.RIGHT, expand=True)
     
     def start(self):
@@ -23,7 +25,7 @@ class GUI:
         self.root.geometry("750x450")
         self.root.configure(bg="white")
 
-        loadLabel = tk.Label(self.frameBtn, text="Select a JSON file with the data to operate", bg="white")
+        loadLabel = tk.Label(self.frameBtn, text="Select a JSON file with the data to operate", bg="white", font="Arial 10")
         loadLabel.pack()
 
         loadFileBtn = tk.Button(self.frameBtn, text="Load File", command=self.readFile)
@@ -31,6 +33,8 @@ class GUI:
 
         graphLabel = tk.Label(self.frameGraph, text="Linear Regression Graph", font="Arial 15 bold", bg="white")
         graphLabel.pack(pady=0)
+
+        self.graph("", "", [], [], Regression([], []))
 
         self.root.mainloop()
 
@@ -54,6 +58,7 @@ class GUI:
         self.graph(xName, yName, xData, yData, regression)
 
     def graph(self, xName, yName, xData, yData, regression):
+        self.drawTable(xName, yName, xData, yData, xData[-1] if len(xData) > 0 else 0, yData[-1] if len(yData) > 0 else 0)
         if self.canvas is not None:
             self.canvas.get_tk_widget().destroy()
         
@@ -70,3 +75,35 @@ class GUI:
         ax.set_title("{} vs {}".format(xName, yName))
         ax.set_xlabel(xName)
         ax.set_ylabel(yName)
+
+    def drawTable(self, xName, yName, xData, yData, dataToPredict, predictedData):
+        if self.table is not None:
+            self.table.destroy()
+            self.dataToPredict.destroy()
+            self.predictedData.destroy()
+
+        table = ttk.Treeview(self.frameBtn, columns=(xName, yName), show="headings")
+        table.column(xName, width=100, anchor="center")
+        table.column(yName, width=100, anchor="center")
+        table.heading(xName, text=xName)
+        table.heading(yName, text=yName)
+
+        for i in range(len(xData) - 1):
+            table.insert("", tk.END, values=(xData[i], yData[i]))
+
+        dataToPredictLabel = tk.Text(self.frameBtn, height=1, font="Arial 10", bg="white", width=20, borderwidth=0)
+        dataToPredictLabel.tag_configure("bold", font="Arial 10 bold")
+        dataToPredictLabel.insert(tk.END, "Data to predict: ", "bold")
+        dataToPredictLabel.insert(tk.END, dataToPredict)
+
+        predictedDataLabel = tk.Text(self.frameBtn, height=1, font="Arial 10", bg="white", width=20, borderwidth=0)
+        predictedDataLabel.tag_configure("bold", font="Arial 10 bold")
+        predictedDataLabel.insert(tk.END, "Predicted data: ", "bold")
+        predictedDataLabel.insert(tk.END, predictedData)
+        
+        self.table = table
+        self.dataToPredict = dataToPredictLabel
+        self.predictedData = predictedDataLabel
+        table.pack(pady=30)
+        dataToPredictLabel.pack(pady=10)
+        predictedDataLabel.pack(pady=10)
