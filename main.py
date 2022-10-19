c353a668014fb37944d872aec500a08bf0a60fcc
Jose Cruz <josedanielcrz@gmail.com>
2022-10-19 07:28:14 -0500

    Update

--- main.py
+++ main.py
@@ -13,18 +13,18 @@ def main():
     """gui = GUI()
     gui.start()"""
 
-    data = Json("assets/kmeans/DataTest1.json").read()
+    """data = Json("assets/kmeans/DataTest1.json").read()
 
     rand=random.sample(range(2, len(data)), 1)
     rand.append(1)
     print(rand)
     Kmeans= K_means(data,2,rand)
-    Kmeans.start()
+    Kmeans.start()"""
 
-    """data = Json("assets/id3/DataTest1.json").read()
+    data = Json("assets/id3/DataTest4.json").read()
 
     id3 = Id3(data)
-    id3.start()"""
+    id3.start()
 
     """dataToAsign, data = Json("assets/NaiveBayes/DataTest3.json").read()
 
